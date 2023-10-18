ad0f3ba92f59da5e4adcf4eff40442f679de0238
raffaelecapaldo <raffaelecpl@gmail.com>
2023-10-18 17:59:53 +0200

    Added delete and store (Category) /w templates

--- src/main/java/org/java/app/photoalbum/controller/CategoryController.java
+++ src/main/java/org/java/app/photoalbum/controller/CategoryController.java
@@ -1,14 +1,23 @@
 package org.java.app.photoalbum.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.java.app.photoalbum.db.pojo.Category;
 import org.java.app.photoalbum.db.serv.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/categories")
@@ -20,10 +29,44 @@ public class CategoryController {
 	@GetMapping public String getIndex(Model model) {
 		
 		List<Category> categories = categoryService.findAll();
-		
 		model.addAttribute("categories", categories);
-		
 		return "categories/category-index";
 		
+	}
+	
+	@GetMapping("/create")
+	public String getCreateForm(Model model) {
+		model.addAttribute("category", new Category());
+		return "categories/category-create";
+	}
+	
+	@PostMapping("/create")
+	public String storeCategory(@Valid @ModelAttribute Category category, BindingResult bindingResult, Model model, 
+			RedirectAttributes ra) {
+		if (bindingResult.hasErrors()) {
+			 return "categories/category-create";
+		}
+		else {
+			try {
+				categoryService.save(category);
+			} catch (DataIntegrityViolationException e) {
+				model.addAttribute("nameNotUnique", "Categoria con nome già esistente");
+				 return "categories/category-create";			}
+			ra.addFlashAttribute("updateMessage", "Categoria aggiunta correttamente");
+			return "redirect:/categories";
+		}}
+	
+	@PostMapping("/delete/{id}")
+	public String deleteCategory(@PathVariable int id, RedirectAttributes ra) {
+		Optional<Category> optCategory = categoryService.findById(id);
+		if (optCategory.isEmpty()) {
+			return "redirect:/categories";
+		}
+		Category category = optCategory.get();
+		categoryService.detachPhotos(category);
+		categoryService.delete(category);
+		ra.addFlashAttribute("updateMessage", "Categoria rimossa correttamente");
+
+		return "redirect:/categories";
 	}
 }
--- src/main/java/org/java/app/photoalbum/db/serv/CategoryService.java
+++ src/main/java/org/java/app/photoalbum/db/serv/CategoryService.java
@@ -3,8 +3,8 @@ package org.java.app.photoalbum.db.serv;
 import java.util.List;
 import java.util.Optional;
 
-
 import org.java.app.photoalbum.db.pojo.Category;
+import org.java.app.photoalbum.db.pojo.Photo;
 import org.java.app.photoalbum.db.repo.CategoryRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,4 +26,17 @@ public class CategoryService {
 	public void save(Category category) {
 		categoryRepo.save(category);
 	}
+	
+	public void delete(Category category) {
+		categoryRepo.delete(category);
+	}
+	
+	public void detachPhotos(Category Category) {
+		List<Photo> photos = Category.getPhotos();
+		for (Photo photo : photos) {
+			List<Category> allCategories = photo.getCategories();
+			allCategories.remove(Category);
+			photo.setCategories(allCategories);
+			}
+		}
 }
