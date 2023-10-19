3e38e7f7b6b38b2aee3545c457de406ab4e6184c
raffaelecapaldo <raffaelecpl@gmail.com>
2023-10-19 12:24:02 +0200

    Added getPhotos, implemented in template

--- frontendVue/src/App.vue
+++ frontendVue/src/App.vue
@@ -17,43 +17,70 @@
       <h1 class="text-uppercase">Le foto più apprezzate</h1>
     </div>
     <div class="container-fluid d-flex gap-3 flex-wrap justify-content-center">
-        <div v-for="x in 5" class="container-photo">
-          <div class="photo">
-            <div class="photo-image ">
-              <div class="d-flex justify-content-center">
-              <img class="img-fluid photo" src="https://i1.adis.ws/i/canon/pro-issues-affecting-student-photographers-1_4961dcc710a54b35b57d7c1dc2c9a14f?$media-collection-full-dt-jpg" alt="">
+      <div v-for="photo in photos" class="container-photo">
+        <div class="photo">
+          <div class="photo-image ">
+            <div class="d-flex justify-content-center">
+              <img class="photo img-fluid" :src="photo.url" :alt="photo.name">
             </div>
-              <div class="info-image pt-1 d-flex flex-column align-items-center">
-                <h3>Title</h3>
-                <p>Descrizione</p>
-              </div>
-              <div class="extra-info">
-                <p class="m-0">Categorie: </p>
-                <p>Autore: </p>
-              </div>
+            <div class="info-image pt-1 d-flex flex-column align-items-center">
+              <h3>{{ photo.title }}</h3>
+              <p>{{ photo.description }}</p>
+            </div>
+            <div class="extra-info">
+              <p v-if="photo.categories" class="m-0">Categorie:
+                <span v-for="(category, i) in photo.categories">{{ category.name }}<span
+                    v-if="i < photo.categories.length - 1">, </span></span>
+              </p>
+              <!-- TODO -->
+              <!-- <p>Autore: </p> -->
             </div>
           </div>
         </div>
       </div>
+    </div>
   </main>
 </template>
 
 <script>
-  export default {
-    
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      API_URL: "http://localhost:8080/api/v1/",
+      photos: null
+    }
+  },
+  methods: {
+    getPhotos() {
+      axios.get(this.API_URL + 'photos')
+        .then((res) => {
+          this.photos = res.data
+        })
+        .catch((e) => {
+          console.log(e)
+        })
+    }
+  },
+  mounted() {
+    this.getPhotos()
+    console.log(this.photos)
   }
+
+}
 </script>
 
 <style lang="scss" scoped>
-
 nav {
-  padding:15px;
+  padding: 15px;
   background-color: #1b292b;
-  color:white;
+  color: white;
+
   li a {
-    color:white;
+    color: white;
     text-decoration-line: none;
     font-size: 30px;
+
     &:hover {
       text-decoration-line: underline;
     }
@@ -66,25 +93,28 @@ nav {
 }
 
 .hero {
-  background-color:#46576b;
-  color:#F2F2F2
+  background-color: #46576b;
+  color: #F2F2F2
 }
 
 .photo-image {
-  padding:10px;
-  color:#F2F2F2;
+  padding: 10px;
+  color: #F2F2F2;
   background-color: #1b292b;
+  min-height: 400px;
+  max-height: 400px;
+
   .photo {
-    border:1px solid white
+    border: 1px solid white;
+    height: 200px;
+    object-fit: cover;
+
   }
 }
 
 .container-photo {
-  min-width: 320px;
-  max-width: 320px;
+  min-width: 350px;
+  max-width: 350px;
+
 }
-
-
-
-
 </style>
